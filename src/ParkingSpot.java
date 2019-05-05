203d59fcf05222d8ee2c68bab0bfa069ab709123
Jianghong Ying <YJianghong@gmail.com>
2019-05-05 19:34:31 -0400

    add some files

---
+++ src/ParkingSpot.java
@@ -0,0 +1,12 @@
+public class ParkingSpot {
+    private final VehicleSize size;
+    private Vehicle currentVehicle;
+
+    ParkingSpot(VehicleSize size){
+        this.size = size;
+    }
+
+    boolean fit(Vehicle v) {
+        return currentVehicle == null && size.getSize() >= v.getSize().getSize();
+    }
+}
